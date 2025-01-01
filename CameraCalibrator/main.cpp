4c6a0e9c828c5a4d166ca836e65e9ec19018d9b5
joao-gueifao-924 <1582944+joao-gueifao-924@users.noreply.github.com>
2025-01-01 19:27:14 -0300

    whitespace changes

--- CameraCalibrator/main.cpp
+++ CameraCalibrator/main.cpp
@@ -13,56 +13,62 @@ static std::string INPUT_IMAGE{ "Input image" };
 
 int main(int argc, char** argv)
 {
-	using namespace std;
-	using namespace cv;
+    using namespace std;
+    using namespace cv;
 
-	cout << "Hello World!" << endl;
-	cout << "OpenCV version: " << CV_VERSION << endl;
-	//std::cout << "Trying to load image: " << IMAGE_PATH << std::endl;
+    cout << "Hello World!" << endl;
+    cout << "OpenCV version: " << CV_VERSION << endl;
+    //std::cout << "Trying to load image: " << IMAGE_PATH << std::endl;
 
-	//cv::Mat image = cv::imread(IMAGE_PATH);
-	cv::namedWindow(INPUT_IMAGE, cv::WINDOW_KEEPRATIO);
-	//cv::imshow(INPUT_IMAGE, image);
-	//cv::waitKey(0);
+    //cv::Mat image = cv::imread(IMAGE_PATH);
+    cv::namedWindow(INPUT_IMAGE, cv::WINDOW_KEEPRATIO);
+    //cv::imshow(INPUT_IMAGE, image);
+    //cv::waitKey(0);
 
-	VideoCapture cap{ 0 };
-	Mat frame_bgr, frame_bgr_flipped, frame_gray_flipped;
+    VideoCapture cap{ 0 };
+    Mat frame_bgr, frame_bgr_flipped, frame_gray_flipped;
 
 
-	cap.set(CAP_PROP_FRAME_WIDTH, 1280);
-	cap.set(CAP_PROP_FRAME_HEIGHT, 720);
+    cap.set(CAP_PROP_FRAME_WIDTH, 1280);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 720);
 
-	// Remember that Fast Check erroneously fails with high distortions like fisheye.
-	const int chessboard_flags = CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK;
-	vector<Point2f> corners;
-	const cv::Size window_search_size{ 11, 11 };
-	const cv::Size chessboard_size{ 9,6 };
+    // Remember that Fast Check erroneously fails with high distortions like fisheye.
+    const int chessboard_flags =  CALIB_CB_ADAPTIVE_THRESH 
+                                | CALIB_CB_NORMALIZE_IMAGE 
+                                | CALIB_CB_FAST_CHECK;
+    vector<Point2f> corners;
+    const cv::Size window_search_size{ 11, 11 };
+    const cv::Size chessboard_size{ 10,7 };
 
-	while (true)
-	{
-		cap >> frame_bgr;
+    while (true)
+    {
+        cap >> frame_bgr;
 
-		cv::flip(frame_bgr, frame_bgr_flipped, 1);
+        cv::flip(frame_bgr, frame_bgr_flipped, 1);
 
-		if (frame_bgr.empty())
-		{
-			throw std::runtime_error("Empty video frame.");
-		}
+        if (frame_bgr.empty())
+        {
+            throw std::runtime_error("Empty video frame.");
+        }
 
-		bool found = findChessboardCorners(frame_bgr_flipped, chessboard_size, corners, chessboard_flags);
+        bool found = findChessboardCorners(frame_bgr_flipped, chessboard_size, 
+                                           corners, chessboard_flags);
 
-		if (found)
-		{
-			cvtColor(frame_bgr_flipped, frame_gray_flipped, COLOR_BGR2GRAY);
-			cornerSubPix(frame_gray_flipped, corners, window_search_size,
-				Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 30, 0.0001));
+        if (found)
+        {
+            cvtColor(frame_bgr_flipped, frame_gray_flipped, COLOR_BGR2GRAY);
+            
+            cornerSubPix(frame_gray_flipped, corners, window_search_size,
+                         Size(-1, -1), 
+                         TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 
+                                      30, 0.0001));
 
-			drawChessboardCorners(frame_bgr_flipped, chessboard_size, corners, found);
-		}
+            drawChessboardCorners(frame_bgr_flipped, chessboard_size, corners, found);
+        }
 
-		cv::imshow(INPUT_IMAGE, frame_bgr_flipped);
-		cv::waitKey(1);
-	}
+        cv::imshow(INPUT_IMAGE, frame_bgr_flipped);
+        cv::waitKey(1);
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
